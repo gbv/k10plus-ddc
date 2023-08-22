37d82e9a7e8fdbd2fe475580a082af1b1ca8567d
Jakob Voss <voss@gbv.de>
2023-08-22 10:01:44 +0200

    Update k10plus-patch.js

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 # K10plus DDC
 
-This repository contains scripts to analyze, convert and publish Dewey Decimal Classification (DDC) numbers found K10plus catalog. The analysis is mainly based on [coli-ana].
+This repository contains scripts to analyze, convert and publish Dewey Decimal Classification (DDC) numbers found K10plus catalog. The analysis is mainly based on [coli-ana] DDC number decomposition.
 
 [coli-ana]: https://github.com/gbv/coli-ana
 [K10plus Subjects]: https://github.com/gbv/k10plus-subjects
 [jskos]: https://format.gbv.de/jskos
 
-## Overview
+## Usage
 
-Scripts:
+### Installation
+
+~~~sh
+npm ci
+~~~
+
+### bin/k10plus-patch.js
+
+This script reads PICA+ records or PPNs, extracts DDC fields, retrieves DDC
+analysis and emits PICA Patch files to insert result into the records.
+
+### analyze.js
 
 - `analyze.js` - read a list of (numerically sorted) DDC numbers and generate analysis with coli-ana 
 - `count.js` - calculate frequency of individual DDC elements in analysis result and emit as CSV or JSKOS concept list
 
-Data:
+## Data in this repository
 
 - `ddcs` sorted DDC numbers found in K10plus with number of occurrences. Data generated from [K10plus Subjects].
-
-## Usage
-
-~~~sh
-npm ci
-node analyze.js
-node count.js
-~~~
 
 ## See Also
 
---
+++ bin/k10plus-patch.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import fs from "fs"
+import { program } from "commander"
+import { Patcher } from "../lib/patcher.js"
+import { Transform, Readable } from "stream"
+import { parseStream, serializePica, getPPN } from "pica-data"
+import split2 from "split2"
+import axios from "axios"
+
+program
+  .name('k10plus-patch')
+  .description('Check and extend DDC numbers in PICA records of K10plus catalogue')
+  .option('-a, --api <URL>', 'coli-ana API endpoint')
+  .option('-c, --continue <ppn>', 'continue after given PPN (expect sorted)')
+  .option('-f, --format <name>', 'PICA+ serialization (default: plain)')
+  .option('-i, --input <file>', 'input file (default: - for STDIN)')
+  .option('-p, --ppns', 'input is list of PPNs instead of PICA records')
+  .action(action)
+
+await program.parseAsync(process.argv)
+
+async function action(opts) {
+  const ppnValue = ppn => (parseInt((ppn || "").slice(0,-1)) || 0)
+
+  const startPPN = ppnValue(opts.continue)
+  const input = (opts.input || '-') == "-" ? process.stdin : fs.createReadStream(opts.input)
+  const format = opts.format || "plain"
+
+  const patcher = new Patcher(opts)
+  const patchTransform = new Transform({
+    writableObjectMode: true,
+    readableObjectMode: true,
+    transform: (record, encoding, next) => {
+      if (ppnValue(getPPN(record)) > startPPN) {
+        patcher.patchRecord(record).then(result => next(null, result))
+      } else {
+        next(null)
+      }
+    }
+  })
+
+  var recordStream
+
+  if (opts.ppns) {
+    recordStream = input.pipe(split2("\n")).pipe(new Transform({
+      readableObjectMode: true,
+      transform: (ppn, encoding, next) => {
+        if (`${ppn}`.match(/^[0-9]+[0-9Xx]$/)) {
+          const id = `opac-de-627!xpn=online!levels=0:ppn:${ppn}`
+          axios.get("https://unapi.k10plus.de/", { params: { id, format: "picajson" } })
+          .catch(() => console.error(`unAPI request failed with PPN ${ppn}`))
+          .then(result => next(null, result?.data || []))
+        } else {
+          next(null, [])
+        }
+      }
+    })) 
+  } else {
+    recordStream = parseStream(input, { format })
+  }
+
+  recordStream.pipe(patchTransform)
+  .on("error", ({message, line}) => console.error(`${message} on line ${line}`))
+  .on("data", patch => {
+    if (patch.length) {
+      console.log(serializePica(patch, { format: 'annotated' }))
+    }
+  })
+}
